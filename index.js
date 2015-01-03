5731a283a797ebd4e57066689a78972a2c7a92da
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2015-01-03 02:43:00 +0100

    Updated to use node-mount

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var chroot = require('posix').chroot;
 var rimraf = require('rimraf').sync;
 
-var mount = require('src-mount');
+var mount = require('nodeos-mount');
 
 var utils = require('nodeos-mount-utils');
 
@@ -12,62 +12,79 @@ const pathRootfs    = '/.rootfs';
 const pathOverlayfs = '/.overlayfs';
 
 
+function onerror(error)
+{
+  // Error mounting the root filesystem or executing init, enable REPL
+  console.warn(error)
+  utils.startRepl('NodeOS-mount-rootfs')
+}
+
 function overlayfsroot(envDev)
 {
   var flags  = mount.flags.MS_NODEV | mount.flags.MS_NOSUID;
 
   // Mount root filesystem
-  var type   = 'ext4' //process.env.ROOTFSTYPE || 'auto';
-  var extras = 'errors=remount-ro';
+  var type   = process.env.ROOTFSTYPE || 'auto';
+  var extras = {errors: 'remount-ro'};
 
   utils.mountfs(envDev, pathRootfs, type, flags, extras, function(error)
   {
-    if(!error)
+    if(error) return onerror(error)
+
+    // Craft overlayed filesystem
+    var type   = 'overlay';
+//      var extras = {lowerdir: pathRootfs};
+    var extras =
     {
-      // Craft overlayed filesystem
-      var type   = 'overlay';
-//      var extras = 'lowerdir='+pathRootfs+':/';
-      var extras = 'lowerdir=/,upperdir='+pathRootfs+'/rootfs,workdir='+pathRootfs+'/workdir';
+      lowerdir: '/',
+      upperdir: pathRootfs+'/rootfs',
+      workdir : pathRootfs+'/workdir'
+    };
 
-      var res = utils.mkdirMount('', pathOverlayfs, type, extras);
-//      var res = utils.mkdirMount('', pathOverlayfs, type, mount.flags.MS_RDONLY, extras);
-      if(res == 0)
+    utils.mkdirMount('', pathOverlayfs, type, extras, function(error)
+//      utils.mkdirMount('', pathOverlayfs, type, mount.flags.MS_RDONLY, extras, function(error)
+    {
+      if(error) return onerror(error)
+
+      var path  = '/';
+
+      // Re-mount initram as read-only
+      var flags = mount.flags.MS_REMOUNT | mount.flags.MS_RDONLY;
+
+      mount.mount('', path, flags, function(error)
       {
-        var path  = '/';
-
-        // Re-mount initram as read-only
-        var flags = mount.flags.MS_REMOUNT | mount.flags.MS_RDONLY;
-
-        var res = mount.mount('', path, '', flags, '');
-        if(res == -1) console.error('Error re-mounting '+path+' as read-only')
+        if(error)
+        {
+          console.error('Error re-mounting '+path+' as read-only')
+          return onerror(error)
+        }
 
         // Move kernel filesystems to overlayed filesystem
-        mount.mount('/dev' , pathOverlayfs+'/dev' , '', mount.flags.MS_MOVE);
-        mount.mount('/proc', pathOverlayfs+'/proc', '', mount.flags.MS_MOVE);
-//        mount.mount('/sys', pathOverlayfs+'/sys' , '', mount.flags.MS_MOVE);
-        mount.mount('/tmp' , pathOverlayfs+'/tmp' , '', mount.flags.MS_MOVE);
+        mount.mount('/dev' , pathOverlayfs+'/dev' , mount.flags.MS_MOVE);
+        mount.mount('/proc', pathOverlayfs+'/proc', mount.flags.MS_MOVE);
+//        mount.mount('/sys' , pathOverlayfs+'/sys' , mount.flags.MS_MOVE);
+        mount.mount('/tmp' , pathOverlayfs+'/tmp' , mount.flags.MS_MOVE);
 
         // Move overlayed filesytem to /
         process.chdir(pathOverlayfs)
-        var res = mount.mount('.', path, '', mount.flags.MS_MOVE);
-        if(res == 0)
+        mount.mount('.', path, mount.flags.MS_MOVE, function(error)
         {
+          if(error)
+          {
+            console.error('Error moving overlayed filesystem to /')
+            return onerror(error)
+          }
+
           chroot('.')
 
           // Execute init
           var argv = process.argv.slice(2)
           var error = utils.execInit('/root', argv)
 //          var error = utils.execInit(path, argv)
-          if(!error) return;
-        }
-
-        console.error('Error moving overlayed filesystem to /')
-      }
-    }
-
-    // Error mounting the root filesystem or executing init, enable REPL
-    console.warn(error)
-    utils.startRepl('NodeOS-mount-rootfs')
+          if(error) onerror(error);
+        });
+      });
+    });
   });
 }
 
@@ -76,9 +93,9 @@ function overlayfsroot(envDev)
 process.umask(0066);
 
 // Remove from rootfs the files only needed on boot to free memory
-rimraf('/bin')
+//rimraf('/bin')
 rimraf('/init')
-rimraf('/lib/node_modules')
+//rimraf('/lib/node_modules')
 rimraf('/sbin')
 
 // Mount kernel filesystems
